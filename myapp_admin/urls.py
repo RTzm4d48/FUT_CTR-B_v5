88e7945f7c683f3ba3f9554ff091466581b1acea
Edgar-ae <edgar.aggre@gmail.com>
2023-06-04 23:03:30 -0500

    some thing chanses

--- myapp_admin/urls.py
+++ myapp_admin/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('ils_admin', views.ilsadmin, name="n_ilsadmin"),
-    path('ils_admin/staff', views.treasury,name="n_staff"),
+    path('ils_admin/staff', views.staff,name="n_staff"),
     path('ils_admin/staff/fut', views.view_fut, name="n_view_fut"),
     path('viewsend/', views.view_send, name='view_send'),
     path('ils_admin/staff/fut_postulated', views.postulated, name="n_postulated"),
     path('ils_admin/staff/fut_pending', views.pending, name="n_pending"),
-    path('ils_admin/staff/fut_send', views.send, name="n_send")
+    path('ils_admin/staff/fut_send', views.send, name="n_send"),
+    path('send_01/', views.send_01_treasurer, name="n_send_01")
 ]
--- myapp_admin/views.py
+++ myapp_admin/views.py
@@ -5,33 +5,50 @@ from myapp.models import fut
 def ilsadmin(request):
     return render(request, 'ils_admin.html')
 
-def treasury(request):
-    print('jelouda locoooo')
-    objs = fut.objects.exclude(stage__exact=3).values('order', 'reason', 'name', 'dni', 'code', 'stage')
+def staff(request):
+    objs = fut.objects.exclude(stage__exact=3).values('id', 'order', 'reason', 'name', 'dni', 'code', 'stage', 'view')
 
     #names_short = [str(ob['order'])[:6] for ob in objs]
     
     #modifico el numero de caracteres del los datos de los diccionarios y los agrego a la lista 'list_data'
+
+    # Esto para saber cuantos fut's no esta leídos y el numero totoal de fut's
+    num_no_view = 0
+    total_futs = 0
+    for i in objs:
+        total_futs = total_futs + 1
+        if i['view'] == 0:
+            num_no_view = num_no_view + 1
+    
     list_data = []
     for i in objs:
         diccionary={}
+        diccionary['id'] = i['id']
         diccionary['order'] = i['order'][:30]
         diccionary['reason'] = i['reason'][:40]
         diccionary['name'] = i['name'][:50]
         diccionary['code'] = i['code']
         diccionary['stage'] = i['stage']
+        diccionary['view'] = i['view']
         list_data.append(diccionary)
         
         #reason.append(i['reason'][:40])
     len_list_data = len(list_data)
     return render(request, 'admin/staff.html', {
         'Object': list_data,
-        'Len_list_data': len_list_data
+        'Len_list_data': len_list_data,
+        'views': num_no_view,
+        'total_futs': total_futs
     })
 
 def view_fut(request):
     code_ = request.GET.get('code')
     objs = fut.objects.filter(code=code_).values('id', 'myrequest', 'name', 'program', 'phone', 'dni', 'cycle', 'reason', 'email', 'order', 'date').first()
+
+    # actualizamos el campo view
+    up_register = fut.objects.get(code=code_)
+    up_register.view = 1
+    up_register.save()
 
     return render(request, 'admin/staff/view_fut.html', {
         'code': code_,
@@ -43,6 +60,7 @@ def view_send(request):
         id = request.GET.get('id')
         stage_ = request.GET.get('stage')
 
+        #actualizamos el campo stage del modelo fut
         up_register = fut.objects.get(id=id)
         up_register.stage = stage_
         up_register.save()
@@ -114,3 +132,18 @@ def send(request):
         'Object': list_data,
         'Len_list_data': len_list_data
     })
+
+def send_01_treasurer(request):
+    ticket = request.GET.get('ticket')
+    id = request.GET.get('id')
+
+    print(ticket)
+    print(id)
+
+    #actualizamos el campo stage del modelo fut
+    up_register = fut.objects.get(id=id)
+    up_register.n_ticket = ticket
+    up_register.save()
+
+    message = 'successful'
+    return JsonResponse({'message': message})
