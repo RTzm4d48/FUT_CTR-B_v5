34072f6b8c62d6686ec092c0b99b142095dfac59
Edgar-ae <edgar.aggre@gmail.com>
2023-11-05 16:49:28 -0500

    ordené las vistas

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,30 +1,33 @@
 from django.urls import path, include
 from . import views # corto y pego de ./mysite/urls.py
+from . import vws_createfut_process
+from . import vws_view_fut
 
 urlpatterns = [
     path('', views.index, name="n_home"), # corto y pego de ./mysite/urls.py
-    path('form_new_fut/identification', views.form_new_fut, name="n_new_fut"), #corto y pego de ./mysite/urls.py
+    
     path('my_fut', views.my_fut),
     
-    
+    # views.py AQUI ESTAN TODAS LA DIRECCIONES PARA CREAR EL FUT
+    path('form_new_fut/identification', views.form_new_fut, name="n_new_fut"),
     path('form_new_fut/processtd', views.create_fut_process, name="n_process"),
     path('form_new_fut/pay', views.create_fut_pay, name="n_pay"),
-    path('form_new_fut/wait_payment', views.create_fut_wait, name="n_wait"),
     path('form_new_fut/finisher', views.finisher, name="n_end"),
     path('form_new_fut/successful', views.successful, name="n_successful"),
 
-    path('my_fut/proceedings', views.proceedings, name="n_proceedings"),
+    # vws_createfut_process AQUI GESTIONAMOS TODO SOBRE LA CREACION DEL FUT
+    path('my_fut/proceedings', vws_createfut_process.proceedings, name="n_proceedings"),
+    path('my_fut/download_image', vws_createfut_process.download_image, name="n_img_dow"),
+    path('my_fut/my_credentials_EMAIL', vws_createfut_process.my_credentials_email, name="n_send_email"),
+    path('send_email/', vws_createfut_process.send_email, name='n_send_email'),
+    path('procedures_list/', vws_createfut_process.procedures_list, name='n_procedures_list'),
 
-    path('my_fut/download_image', views.download_image, name="n_img_dow"),
-    path('my_fut/my_credentials_EMAIL', views.my_credentials_email, name="n_send_email"),
-
-    path('send_email/', views.send_email, name='n_send_email'),
-    path('procedures_list/', views.procedures_list, name='n_procedures_list'),
     # login
     path('logout/', views.exit, name='n_exit'),
 
     # view fut
-    path('my_fut/in-progress', views.view_fut_in_progress, name='n_in_progress'),
-    path('my_fut/finished', views.view_fut_finished, name='n_finished'),
+    #vws_view_fut AQUI ESTARA TODAS LAS VITAS PARA VER EL TRAMITE REALIZADO
+    path('my_fut/in-progress', vws_view_fut.view_fut_in_progress, name='n_in_progress'),
+    path('my_fut/finished', vws_view_fut.view_fut_finished, name='n_finished'),
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -13,9 +13,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
-#pip install qrcode
-import random, qrcode
-
 #pip install opencv-python-headless
 import cv2
 import base64
@@ -127,16 +124,7 @@ def create_fut_pay(request):
     else:
         return HttpResponse("<h1>404 Not Found :(</h1>")
 
-# para generear el expediente
-async def generate_proceedings():
-    # Genramos el número de Expediente
-    Expediente = random.sample(range(0, 9),5)
-    exp_ = ''.join(map(str, Expediente))
-    # Generamos la contraseña
-    Contraseña = random.sample(range(0, 9),4)
-    pas_ = ''.join(map(str, Contraseña))
 
-    return exp_, pas_
 
 
 async def generate_code():
@@ -305,190 +293,6 @@ def successful(request):
         'Proceeding': objetos['proceeding'],
         'Password': objetos['password']
     })
-
-def proceedings(request):
-    code_ = request.GET.get('code')
-    object = fut.objects.filter(code=code_).values('id', 'name', 'dni', 'order', 'proceeding', 'password', 'code', 'program', 'email').first()
-    dni = str(object['dni'][:3])
-    #Email process
-    email = str(object['email'])
-    e = email[0]
-    l = email[-11]
-    nummail = len(email)
-    nummail = nummail-12 # (10 de @gmail.com) (2 de los dos digitos que si se ven)
-
-    #generamos los asteriscos
-    asterisk = []
-    for i in range(nummail):
-        asterisk.append('*')
-    asterisk_Str = ''.join(asterisk)
-    email_code = e+asterisk_Str+l+'@gmail.com'
-
-    # My params for css
-    progressbar = list(range(9)) # for progress bar lines
-
-    details = []
-  
-    x = 20
-    for i in range(4):# aquí ponemos el numero de cuadros de detalles que habra en la interfaz
-        details.append(x)
-        x += 20
-    # LO QUE VENDRA DE UNA BASE DE DATOS:
-    my_id = 1
-    loco = []
-    
-    num_registros = process.objects.filter(fut_id_id=object['id']).count()
-    print("Hay {} registros con MiModelo_id igual a 3".format(num_registros))
-
-    for i in range(num_registros):
-        data = process.objects.filter(stage=i, fut_id_id=object['id']).values('tittle', 'name', 'reception', 'exit', 'num').first()
-        loco.append(data)
-    
-
-    left = 20 # css left details picture
-    return render(request, 'view_fut/proceedings.html', {
-        'Name': object['name'],
-        'Dni': dni,
-        'Order': object['order'],
-        'Proceeding': object['proceeding'],
-        'Program': object['program'],
-        'Progressbar': progressbar,
-        'Details': details,
-        'Left': left,
-        'Email_code': email_code,
-        'Email': email,
-        'Password': object['password'],
-        'Data': loco,
-        'Num_process': num_registros
-    })
-
-
-def send_email(request):
-    send_email = request.GET.get('gmail')
-    name = request.GET.get('name')
-    proceeding = request.GET.get('proceeding')
-    password = request.GET.get('password')
-    # Proceso de envio de correo
-    email = 'whiteman.play69@gmail.com'
-    contraseña = 'hkekvgngsirgtjym'
-
-    yag = yagmail.SMTP(user=email, password=contraseña)
-
-    destinatarios = [send_email]
-
-    asunto = 'Credenciales ILS'
-
-    html = f'''
-    <div class="contentsend_father" style="width: 100%;color:black;">
-        <div class="contentsend" style="background: #F0F1F5;width: 90%;display: block;padding: 20px;margin: auto;">
-        <div class="contentlogo" style="text-align: center;">
-            <img width="40%" src="https://i.ibb.co/W0p4JZ2/logoils-01-01.png" alt="">
-        </div>
-        <div class="info" style="text-align: center;font-family: Arial, Helvetica, sans-serif;">
-            <h2>Hola {name}!</h2>
-            <p style="text-align: left;font-size: 15px;">Estas son las credenciales del trámite que se hizo vinculado a esta cuenta de gmail.</p>
-        </div>
-        <div class="contentcredentials" style="width: 80%;margin: auto;padding: 1px;">
-            <div class="contentlog" style="padding: 1px;font-family: Arial, Helvetica, sans-serif;">
-                <div class="proce" style="background: white;display: flex;width: 80%;margin: 8px auto;height: 50px;"><p style="font-size: 16px;margin: auto 2px auto auto;">Expediente: </p><h2 style="margin: auto auto auto 2px;font-size: 16px;">{proceeding}</h2></div>
-                <div class="proce" style="background: white;display: flex;width: 80%;margin: 8px auto;height: 50px;"><P style="font-size: 16px;margin: auto 2px auto auto;">Contraseña</P><h2 style="margin: auto auto auto 2px;font-size: 16px;">{password}</h2></div>
-            </div>
-            <div class="qr" style="width: 150px;height: 150px;margin: 20px auto;">
-                <img style="width: 100%;" src="https://i.ibb.co/V0GrnzK/79-XNZx-F1cu-Aqrcode.png" alt="">
-            </div>
-        </div>
-        <div class="info" style="text-align: center;font-family: Arial, Helvetica, sans-serif;">
-            <p style="text-align: left;font-size: 15px;">Este gmail se ha generado debido a que se usó al realizar un trámite en la página del instituto ILS</p>
-            <p style="text-align: left;font-size: 15px;">Si no estás intentando realizar un trámite en ILS ignora este correo.</p>
-        </div>
-        </div>
-        <div style="width: 80%;margin: 20px auto 0px auto;" class="infopadd">
-        <p style="margin: 0px;font-family: Arial, Helvetica, sans-serif;font-size: small;margin: 10px auto;">Este correo se envió a la dirección de correo electrónico asociado a un trámite en ILS</p>
-        <p style="margin: 0px;font-family: Arial, Helvetica, sans-serif;font-size: small;margin: 10px auto;">Este correo electrónico se genera automáticamente. Por favor no responder a él. Si necesita ayuda adicional. Por favor contacte con nuestro equipo de soporte.</p>
-        <a href="#">www.ils.com/help</a>
-        </div>
-    </div>
-    '''
-    yag.send(destinatarios, asunto, html)
-    # End
-    message = 'successful'
-    return JsonResponse({'message': message})
-
-
-def download_image(request):
-    image_name = 'doc.pdf'
-    image_path = os.path.join(settings.MEDIA_ROOT, image_name)
-    
-    print('-----RUTA DEL PATH-----'+image_path)
-    
-    with open(image_path, 'rb') as f:
-         response = HttpResponse(f.read(), content_type='image/jpeg')
-         response['Content-Disposition'] = 'attachment; filename="%s"' % image_name
-         return response
-    #return render(request, 'view_fut/download_image.html')
-
-def my_credentials_email(request):
-    send_email = 'tapieese500@gmail.com'
-    name = 'Edgar Sanchez'
-    proceeding = '98765'
-    password = '9878'
-
-    email = 'whiteman.play69@gmail.com'
-    contraseña = 'hkekvgngsirgtjym'
-
-    #yag = yagmail.SMTP(user=email, password=contraseña)
-
-    destinatarios = [send_email]
-
-    asunto = 'Credenciales ILS'
-
-    html = f'''
-    <div class="contentsend_father" style="width: 100%;color:black;">
-        <div class="contentsend" style="background: #F0F1F5;width: 90%;display: block;padding: 20px;margin: auto;">
-        <div class="contentlogo" style="text-align: center;">
-            <img width="40%" src="https://i.ibb.co/W0p4JZ2/logoils-01-01.png" alt="">
-        </div>
-        <div class="info" style="text-align: center;font-family: Arial, Helvetica, sans-serif;">
-            <h2>Hola {name}!</h2>
-            <p style="text-align: left;font-size: 15px;">Estas son las credenciales del trámite que se hizo vinculado a esta cuenta de gmail.</p>
-        </div>
-        <div class="contentcredentials" style="width: 80%;margin: auto;padding: 1px;">
-            <div class="contentlog" style="padding: 1px;font-family: Arial, Helvetica, sans-serif;">
-                <div class="proce" style="background: white;display: flex;width: 80%;margin: 8px auto;height: 50px;"><p style="font-size: 16px;margin: auto 2px auto auto;">Expediente: </p><h2 style="margin: auto auto auto 2px;font-size: 16px;">{proceeding}</h2></div>
-                <div class="proce" style="background: white;display: flex;width: 80%;margin: 8px auto;height: 50px;"><P style="font-size: 16px;margin: auto 2px auto auto;">Contraseña</P><h2 style="margin: auto auto auto 2px;font-size: 16px;">{password}</h2></div>
-            </div>
-            <div class="qr" style="width: 150px;height: 150px;margin: 20px auto;">
-                <img style="width: 100%;" src="https://i.ibb.co/V0GrnzK/79-XNZx-F1cu-Aqrcode.png" alt="">
-            </div>
-        </div>
-        <div class="info" style="text-align: center;font-family: Arial, Helvetica, sans-serif;">
-            <p style="text-align: left;font-size: 15px;">Este gmail se ha generado debido a que se usó al realizar un trámite en la página del instituto ILS</p>
-            <p style="text-align: left;font-size: 15px;">Si no estás intentando realizar un trámite en ILS ignora este correo.</p>
-        </div>
-        </div>
-        <div style="width: 80%;margin: 20px auto 0px auto;" class="infopadd">
-        <p style="margin: 0px;font-family: Arial, Helvetica, sans-serif;font-size: small;margin: 10px auto;">Este correo se envió a la dirección de correo electrónico asociado a un trámite en ILS</p>
-        <p style="margin: 0px;font-family: Arial, Helvetica, sans-serif;font-size: small;margin: 10px auto;">Este correo electrónico se genera automáticamente. Por favor no responder a él. Si necesita ayuda adicional. Por favor contacte con nuestro equipo de soporte.</p>
-        <a href="#">www.ils.com/help</a>
-        </div>
-    </div>
-    '''
-    #yag.send(destinatarios, asunto, html)
-    return render(request, 'view_fut/functions/my_credentials_EMAIL.html', {'success': True, 'Email':email})
-
-def procedures_list(request):
-    tipeo = request.GET.get('tipeo')
-    objs = tupa.objects.filter(tipo_de_servicio__startswith=tipeo).order_by('id').values('id', 'tipo_de_servicio')
-    result_list = list(objs)
-    return JsonResponse({'message': result_list})
-
-@login_required
-def view_fut_in_progress(request):
-    return render(request, 'view_fut/futs/in-progress.html')
-
-@login_required
-def view_fut_finished(request):
-    return render(request, 'view_fut/futs/finished.html')
 
 def create_fut_wait(request):
     if request.method == 'POST':
